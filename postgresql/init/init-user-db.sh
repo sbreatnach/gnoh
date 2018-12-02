4f56c6a1e7d2444fc398fcfaabea7729b61e5f80
Shane Breatnach <shane.breatnach@gmail.com>
2018-12-02 11:29:01 +0000

    Integrated LDAP server as federated backend for Keycloak

--- postgresql/init/init-user-db.sh
+++ postgresql/init/init-user-db.sh
@@ -3,12 +3,15 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE DATABASE gitea;
+    CREATE DATABASE keycloak;
     CREATE DATABASE redmine;
     CREATE DATABASE calendars;
     CREATE ROLE gitea LOGIN PASSWORD 'gitea';
+    CREATE ROLE keycloak LOGIN PASSWORD 'keycloak';
     CREATE ROLE redmine LOGIN PASSWORD 'redmine';
     CREATE ROLE calendars LOGIN PASSWORD 'calendars';
     GRANT ALL PRIVILEGES ON DATABASE gitea TO gitea;
+    GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak;
     GRANT ALL PRIVILEGES ON DATABASE redmine TO redmine;
     GRANT ALL PRIVILEGES ON DATABASE calendars TO calendars;
 
